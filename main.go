063b31718e5e529c8eac52ceab8479d1c545d62c
manikeshhhh <manikesh182003@gmail.com>
2025-08-16 01:21:54 +0530

    Test Passed, working for now

--- main.go
+++ main.go
@@ -3,34 +3,46 @@ package main
 import (
 	"fmt"
 	"hash"
+	"time"
 
 	"github.com/spaolacci/murmur3"
 )
 
-var hasher hash.Hash32 = murmur3.New32WithSeed(32)
+var mHasher hash.Hash32
 
-func murmurhash(key string, size int) int {
-	hasher.Write(data)
-	var result = hasher.Sum32()
+func init() {
+	mHasher = murmur3.New32WithSeed(uint32(time.Now().Unix()))
+}
+
+func murmurhash(key string, size int32) int32 {
+	mHasher.Write([]byte(key))
+	result := mHasher.Sum32() % uint32(size)
+	mHasher.Reset()
+	return int32(result)
 
 }
 
 type BloomFilter struct {
 	filter []bool
+	size   int32
 }
 
-func NewBloomFilter(size int) *BloomFilter {
+func NewBloomFilter(size int32) *BloomFilter {
 	return &BloomFilter{
-		make([]bool, size),
+		filter: make([]bool, size),
+		size:   size,
 	}
 }
 
 func (b *BloomFilter) Add(key string) {
+	idx := murmurhash(key, b.size)
+	b.filter[idx] = true
 
 }
 
 func (b *BloomFilter) Exists(key string) bool {
-	return false
+	idx := murmurhash(key, b.size)
+	return b.filter[idx]
 
 }
 
