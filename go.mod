cb531a5a514b12575dc386574989cdbbf45e9091
manikeshhhh <manikesh182003@gmail.com>
2025-08-16 13:39:30 +0530

    false positive rate 0.198

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module bloomfilter
 
 go 1.20
 
-require github.com/spaolacci/murmur3 v1.1.0 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,8 +4,8 @@ package main
 import (
 	"fmt"  // For printing output
 	"hash" // For hash.Hash32 interface
-	"time" // For seeding the hash function
 
+	"github.com/google/uuid"
 	"github.com/spaolacci/murmur3" // MurmurHash3 package
 )
 
@@ -14,7 +14,7 @@ var mHasher hash.Hash32
 
 // init initializes the global hasher with a time-based seed
 func init() {
-	mHasher = murmur3.New32WithSeed(uint32(time.Now().Unix()))
+	mHasher = murmur3.New32WithSeed(uint32(10))
 }
 
 // murmurhash hashes the key and maps it to a valid index in the filter
@@ -43,27 +43,67 @@ func NewBloomFilter(size int32) *BloomFilter {
 func (b *BloomFilter) Add(key string) {
 	idx := murmurhash(key, b.size) // Get index for the key
 	b.filter[idx] = true           // Set the bit at that index
+	//fmt.Println("wrote", key, "at index", idx) // Print the index where the key was added
+}
+
+func (b *BloomFilter) print() {
+	fmt.Println(b.filter)
 }
 
 // Exists checks if a key might be in the Bloom filter
 // Returns true if the bit is set, false otherwise
-func (b *BloomFilter) Exists(key string) bool {
+func (b *BloomFilter) Exists(key string) (string, int32, bool) {
 	idx := murmurhash(key, b.size) // Get index for the key
-	return b.filter[idx]           // Return the bit value
+	return key, idx, b.filter[idx]
+	// Return the bit value
 }
 
 // main demonstrates usage of the Bloom filter
 func main() {
-	bloom := NewBloomFilter(16)     // Create a Bloom filter of size 16
-	keys := []string{"a", "b", "c"} // Keys to add
+	dataset := make([]string, 0) // Initialize dataset to hold UUIDs
+	dataset_exists := make(map[string]bool)
+	dataset_notexists := make(map[string]bool)
+	for i := 0; i < 500; i++ {
+		u := uuid.New()
+		dataset = append(dataset, u.String())
+		dataset_exists[u.String()] = true
+	}
 
-	// Add keys to the filter
-	for _, key := range keys {
+	for i := 0; i < 500; i++ {
+		u := uuid.New()
+		dataset = append(dataset, u.String())
+		dataset_notexists[u.String()] = false
+	}
+
+	bloom := NewBloomFilter(1000)
+
+	for key, _ := range dataset_exists {
 		bloom.Add(key)
+
 	}
 
-	// Check if keys exist in the filter
-	for _, key := range keys {
-		fmt.Println(key, bloom.Exists(key))
+	falsePositive := 0
+	for _, key := range dataset {
+		_, _, exists := bloom.Exists(key)
+		if exists {
+			if _, ok := dataset_notexists[key]; ok {
+				falsePositive++
+			}
+		}
 	}
+	fmt.Println(float64(falsePositive) / float64(len(dataset)))
+
 }
+
+// Create a Bloom filter of size 1000
+//	keys := []string{"a", "b", "c", "d", "e", "f", "g"} // Keys to add
+// Add keys to the filter
+//for _, key := range keys {
+//	bloom.Add(key)
+//	}
+// Check if keys exist in the filter
+//	for _, key := range keys {
+//		fmt.Println(bloom.Exists(key))
+//	}
+//	fmt.Println(bloom.Exists("h"))
+//	bloom.print()
